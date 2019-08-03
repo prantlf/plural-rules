460efad83119b226cd5636c56bdd571a8b7ff78d
Ferdinand Prantl <prantlf@gmail.com>
2019-08-03 10:48:22 +0200

    feat: Provide both minificated and only cleaned-up UMD scripts for the browser

--- rollup.config.js
+++ rollup.config.js
@@ -39,6 +39,21 @@ export default [
       babel({ exclude: 'node_modules/**' }),
       resolve(),
       commonjs(),
+      cleanup()
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.umd.min.js',
+      format: 'umd',
+      name: 'pluralRules',
+      sourcemap: true
+    },
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonjs(),
       uglify()
     ]
   },
@@ -46,6 +61,21 @@ export default [
     input: 'src/code.js',
     output: {
       file: 'dist/code.umd.js',
+      format: 'umd',
+      name: 'pluralRules',
+      sourcemap: true
+    },
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonjs(),
+      cleanup()
+    ]
+  },
+  {
+    input: 'src/code.js',
+    output: {
+      file: 'dist/code.umd.min.js',
       format: 'umd',
       name: 'pluralRules',
       sourcemap: true
